e4ec9cdafed157728b7895833a71b95f8441d845
Brent Ju <brentju03@gmail.com>
2024-05-05 12:55:12 -0700

    WIP: User side reporting flow via bot dm.
    Workflow allows user to initiate a report and identify the abuse as one of 5
    subtypes related to deepfake/generative AI content. Next steps are to implement
    subbranches for each abuse type, and provide a final message to the user for
    them to include their own information.

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -6,7 +6,19 @@ class State(Enum):
     REPORT_START = auto()
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
+    AWAITING_ABUSE_TYPE = auto()   
+     
+    # enums to track unified flow once specific abuse details provided
+    AWAITING_ADDL_INFO = auto()
     REPORT_COMPLETE = auto()
+    
+class AbuseType(Enum):
+    # enums for tracking which flow to take, given type of abuse
+    NSFW = auto()
+    IMPERSONATION = auto()
+    HATEFUL = auto()
+    COPYRIGHT = auto()
+    OTHER = auto()
 
 class Report:
     START_KEYWORD = "report"
@@ -17,6 +29,14 @@ class Report:
         self.state = State.REPORT_START
         self.client = client
         self.message = None
+        self.abuse_types = {
+            1: "nsfw",
+            2: "impersonation",
+            3: "hateful content",
+            4: "copyright infringement",
+            5: "other"
+        }
+        self.cur_abuse_type = None
     
     async def handle_message(self, message):
         '''
@@ -56,10 +76,31 @@ class Report:
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
             return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+                    "Would you like to report the following as:", \
+                    "1. NSFW", "2. Impersonation", "3. Hateful Content", "4. Copyright Infringement", "5. Other"]
         
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            # We are awaiting a reply to the above
+            abuse_type = None
+            msg_key = None
+            try:
+                msg_key = int(message.content)
+                if int(message.content) in self.abuse_types.keys(): 
+                    abuse_type = self.abuse_types[int(message.content)]
+            except ValueError:
+                pass
+            if message.content.lower() in self.abuse_types.values():
+                abuse_type = message.content.lower()
+                
+            if abuse_type:
+                # self.state = State.AWAITING_ADDL_INFO
+                self.cur_abuse_type = abuse_type
+                return [f"I see you have identified the message as {abuse_type}. Please provide some additional information/context to help us address your report."]
+            return ["Sorry, I didn't quite understand. Please try again or say `cancel` to cancel.", \
+                "1. NSFW", "2. Impersonation", "3. Hateful Content", "4. Copyright Infringement", "5. Other"]
+            
+        if self.state == State.AWAITING_ADDL_INFO:
+            return ["TBD: But at least you got to this point!"]
 
         return []
 
