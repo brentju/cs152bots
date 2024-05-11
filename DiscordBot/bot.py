0be0442ac43752f6fdab1b555e96b236c2fea311
Eliot Jones <eliot.k.jones@gmail.com>
2024-05-11 14:55:29 -0700

    idk man

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -147,8 +147,7 @@ class ModBot(discord.Client):
         if author_id not in self.reports and not message.content.startswith(Moderate.START_KEYWORD):
             return
 
-        reference_report = message.channel.history(oldest_first=True, limit=1)
-        print(reference_report)
+        reference_report = await message.channel.history(oldest_first=True, limit=1).flatten()
         reference_report_id = extract_report_id(reference_report.content)
         reported_user, original_message, abuse_type, reporting_user = parse_report_details(reference_report.content)
         if message.author.id not in self.active_replies:
